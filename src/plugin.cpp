05b553fb01b3769895048f8f8a53059cfa73be9c
rodyherrera <contact@rodyherrera.com>
2026-06-05 01:08:36 -0400

    refactor: simplify crystal structure option binding in plugin configuration

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,18 +1,12 @@
 #include <volt/plugin/plugin_entry.h>
 #include <volt/analysis/ptm_service.h>
-#include <volt/structures/crystal_structure_types.h>
 
 using namespace Volt;
 using namespace Volt::Plugin;
 using S = PolyhedralTemplateMatchingService;
 
 static const std::vector<OptionBinding<S>> bindings = {
-    opt<S>({"--crystal_structure", "string", "SC|FCC|HCP|BCC|CUBIC_DIAMOND|HEX_DIAMOND", "FCC"},
-        [](S& s, const OptsMap& opts) {
-            LatticeStructureType t = LATTICE_FCC;
-            parseLatticeStructureType(CLI::getString(opts, "--crystal_structure", "FCC"), t);
-            s.setInputCrystalStructure(t);
-        }),
+    optLattice("--crystal_structure", "SC|FCC|HCP|BCC|CUBIC_DIAMOND|HEX_DIAMOND", "FCC", &S::setInputCrystalStructure),
     opt("--rmsd", "RMSD threshold for PTM", 0.1, &S::setRMSD),
     opt("--dissolve_small_clusters", "Mark small clusters as OTHER", false, &S::setDissolveSmallClusters),
 };
